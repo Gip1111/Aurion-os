f6a824889e40a45ade0cf2307b8e7481a4863e77
Gip1111 <gianalar@gmail.com>
2026-04-23 15:50:09 +0200

    Fix: correct live-build bootloader option syntax to singular --bootloader

--- iso-build/build-iso.sh
+++ iso-build/build-iso.sh
@@ -115,7 +115,7 @@ lb config \
     --iso-volume "AurionOS Alpha 0.1" \
     --linux-flavours generic \
     --linux-packages "linux-image" \
-    --bootloaders "syslinux,grub-efi" \
+    --bootloader "syslinux,grub-efi" \
     --mode ubuntu \
     --system live \
     --apt-recommends false \
