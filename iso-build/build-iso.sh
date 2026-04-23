e8efa6ba0580cc08ef699bbae4bcda66a7bf3ac8
Gip1111 <gianalar@gmail.com>
2026-04-23 16:32:48 +0200

    Fix: set bootloader to syslinux,grub-efi so isohybrid finds the isolinux boot record

--- iso-build/build-iso.sh
+++ iso-build/build-iso.sh
@@ -115,7 +115,7 @@ lb config \
     --iso-volume "AurionOS Alpha 0.1" \
     --linux-flavours generic \
     --linux-packages "linux-image" \
-    --bootloader "grub-efi" \
+    --bootloader "syslinux,grub-efi" \
     --mode ubuntu \
     --system live \
     --apt-recommends false \
